e4579525316bdb516e910d54d471531b7a9c4797
Yuvha <yuvharaajvr@gmail.com>
2026-02-18 17:00:01 +0530

    3rd commit

--- tests/assert_positive_for_payments.sql
+++ tests/assert_positive_for_payments.sql
@@ -1,3 +1,4 @@
-select order_id, sum(AMOUNT) as total_amount from {{ ref('STG_PAYMENTS')}} 
-group by 1
-having not(total_amount >= 0)
+select order_id
+from {{ ref('STG_PAYMENTS') }}
+group by order_id
+having sum(amount) < 0
