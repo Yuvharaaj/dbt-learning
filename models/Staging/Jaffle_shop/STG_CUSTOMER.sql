c8ba6f3f2e2fa1a08de3197df1575b212046c619
Yuvha <yuvharaajvr@gmail.com>
2026-02-19 18:12:52 +0530

    This commit is after rollback to 3rd commit

--- models/Staging/Jaffle_shop/STG_CUSTOMER.sql
+++ models/Staging/Jaffle_shop/STG_CUSTOMER.sql
@@ -2,7 +2,7 @@ with customers as (
     select 
     id as customer_id,
     first_name,
-    last_name from {{source('JAFFLE_SHOP','customers')}}
+    last_name from {{source('JAFFLE_SHOP_RAW_CO','customers')}}
 )
 
 select * from customers
--- models/Staging/Stripe/STG_PAYMENTS.sql
+++ models/Staging/Stripe/STG_PAYMENTS.sql
@@ -4,4 +4,4 @@
     PAYMENTMETHOD as payment_method,
     STATUS,
     {{cent_to_dollar('amount')}} as AMOUNT,
-    CREATED as created_at from {{source('Stripe','payment')}}
+    CREATED as created_at from {{source('JAFFLE_SHOP_RAW_STRIPE','payment')}}
